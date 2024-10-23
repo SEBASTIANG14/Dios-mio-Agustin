6f5e9c7aed1b8d4fd99b2dd99c001b03b86649c3
Sebastian <sebaseboruko@gmail.com>
2024-10-22 21:50:03 -0700

    First commit project

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+import { View } from 'react-native';
 import { Home } from './screens/Home';
+import { Login } from './screens/Login';
 
 export default function App() {
   return (
-    <Home/>
+    <View>
+      {/*<Home/>*/}
+      <Login/>
+    </View>
   );
 }
