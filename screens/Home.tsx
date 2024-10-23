5c41bd415c48536fb35fd2e8e2ff110bbacf0bd2
Doncan00 <90412543+Doncan00@users.noreply.github.com>
2024-10-22 22:32:38 -0600

    Primer push, creada la pantalla Home

---
+++ screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, View, Text } from 'react-native'
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>
+        HOLA
+      </Text>
+    </View>
+  )
+}
